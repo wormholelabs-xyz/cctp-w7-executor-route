ccf8764ef9c7f6a9aeb9b1765b6ae99a86e17689
kev1n-peters <96065607+kev1n-peters@users.noreply.github.com>
2026-05-05 13:27:07 -0500

    fix: allow off-curve owners in getAssociatedTokenAddressSync (#67)
    Squads multisig wallets are PDAs (off-curve), causing
    TokenOwnerOffCurveError. Pass allowOwnerOffCurve=true.

--- src/svm/executorV2.ts
+++ src/svm/executorV2.ts
@@ -150,7 +150,7 @@ export class SvmCCTPv2Executor<N extends Network, C extends SolanaChains>
     );
 
     const senderPk = new SolanaAddress(sender).unwrap();
-    const senderAta = getAssociatedTokenAddressSync(usdc, senderPk);
+    const senderAta = getAssociatedTokenAddressSync(usdc, senderPk, true);
     const referrer = new SolanaAddress(
       details.referrer?.address?.toString() ?? senderPk
     ).unwrap();
@@ -446,7 +446,8 @@ export class SvmCCTPv2Executor<N extends Network, C extends SolanaChains>
 
     const feeRecipientTokenAccount = getAssociatedTokenAddressSync(
       solanaUsdcAddress,
-      tokenMessengerData.feeRecipient
+      tokenMessengerData.feeRecipient,
+      true,
     );
 
     const accountMetas: AccountMeta[] = [
